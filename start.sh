f435aa10f1f9de5a32230a05d6aa62c10f749802
Scanstox <hi.scanstox@gmail.com>
2026-03-12 17:22:39 +0530

    Switch to Python runtime instead of Docker

--- start.sh
+++ start.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 # Startup script for Groww Reviews Weekly Pulse
 
+cd /opt/render/project/src
+
 # Start web server in background
 python -m uvicorn backend.app.main:app --host 0.0.0.0 --port 8000 &
 
